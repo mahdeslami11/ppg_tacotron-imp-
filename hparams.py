4821939763739ded84643b631f55f6cac83d647b
miaomiaomiao-LJY <48284713+miaomiaomiao-LJY@users.noreply.github.com>
2021-04-11 00:04:32 +0800

    ADD
    # Add super parameter module:
    #    Preliminarily set the TIMIT dataset parameters
    #    Preliminarily set the training parameters of net 1

---
+++ hparams.py
@@ -0,0 +1,10 @@
+# TIMIT
+timit_sr = 16000
+timit_n_mfcc = 40
+timit_n_fft = 1024
+timit_hop_length = 256
+timit_wim_length = 1024
+timit_n_mels = 80
+
+# Net1
+net1_train_dataset = "./data/dataset/TIMIT/TRAIN/*/*/*.wav"
