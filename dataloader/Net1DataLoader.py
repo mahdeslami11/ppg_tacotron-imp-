b71c729a97335895fc355048d9edeb2bfdb5123a
miaomiaomiao-LJY <48284713+miaomiaomiao-LJY@users.noreply.github.com>
2021-04-11 16:36:05 +0800

    UPDATE
    # Update the Net1DataLoader module:
    #     Implement the Net1DataSet data loader
    
    # Update the hparams module:
    #     Add net1 train batch_size, num_workers

--- dataloader/Net1DataLoader.py
+++ dataloader/Net1DataLoader.py
@@ -1,5 +1,7 @@
 import glob
-from torch.utils.data import Dataset
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 from audio_operation import get_mfccs_and_phones
 
@@ -15,3 +17,18 @@ class Net1Dataset(Dataset):
 
     def __len__(self):
         return len(self.wav_files)
+
+
+def get_net1_data_loader(data_path, batch_size, num_workers):
+    dataset = Net1Dataset(data_path)
+
+    # worker_init_fn = lambda x: np.random.seed((torch.initial_seed()) % (2 ** 32))
+
+    data_loader = DataLoader(dataset,
+                             batch_size=batch_size,
+                             shuffle=True,
+                             num_workers=num_workers,
+                             drop_last=True,
+                             worker_init_fn=np.random.seed((torch.initial_seed()) % (2 ** 32)))
+
+    return data_loader
--- hparams.py
+++ hparams.py
@@ -10,3 +10,5 @@ timit_default_duration = 2
 
 # Net1
 net1_train_dataset = "./data/dataset/TIMIT/TRAIN/*/*/*.wav"
+net1_train_batch_size = 32
+net1_train_num_workers = 4
